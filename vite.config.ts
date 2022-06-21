e2c7c173bde548f3cbb36105c0b6e1cb9b6dee0b
hanna 🍌 <hanna@iamhanna.de>
2022-06-21 13:56:30 +0200

    Add and configure emotion js

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// Emotion config from https://dev.to/glocore/configure-emotion-with-your-vite-react-project-7jl
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   server: {open: true},
 });
