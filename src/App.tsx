863625ab7171d55b787e5d829678eee12f43a7a9
hanna 🍌 <hanna@iamhanna.de>
2022-06-21 15:18:18 +0200

    Basic layout and unit switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
 import {css} from '@emotion/react';
+import {useState} from 'react';
+import Headroom from 'react-headroom';
+
+const options = ['100vh', 'vmax', 'lvh', 'svh', 'dvh'];
 
 export const App = () => {
+  const [color, setColor] = useState<'salmon' | 'darkseagreen'>('salmon');
+  const [unit, setUnit] = useState<string>('100vh');
   return (
     <div
       css={css`
-        background-color: mintcream;
+        background-color: ${color};
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        height: ${unit};
       `}
     >
-      <header>
-        <p>Hello Vite + React!</p>
-
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-      </header>
+      <Headroom
+        css={css`
+          background-color: #eee;
+          flex: 0 0 auto;
+          padding: 10px 20px;
+        `}
+      >
+        <header>
+          <p>A header bar that hides on scroll</p>
+        </header>
+      </Headroom>
+      <div
+        css={css`
+          flex: 1 0 auto;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <p>This should take the available height</p>
+        <p>Current unit: {unit}</p>
+        <select onChange={(e) => setUnit(e.target.value)}>
+          {options.map((o) => (
+            <option key={o} value={o}>
+              {o}
+            </option>
+          ))}
+        </select>
+      </div>
+      <footer
+        css={css`
+          flex: 0 0 auto;
+          padding: 10px 20px;
+          background-color: #eee;
+        `}
+      >
+        <p>Always in the bottom</p>
+        <button onClick={() => setColor(color === 'salmon' ? 'darkseagreen' : 'salmon')}>Change bg color</button>
+      </footer>
     </div>
   );
 };
